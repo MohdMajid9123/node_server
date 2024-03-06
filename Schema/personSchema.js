d8cd6bfcccb09fa52a35aa1ec58ccd6b3c7769a2
Md <mohdmajid9123@gmail.com>
2024-03-06 16:36:15 +0530

    add passport.js and add two new data that is username and password

--- Schema/personSchema.js
+++ Schema/personSchema.js
@@ -28,6 +28,14 @@ const personSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const personModel = mongoose.model("Person", personSchema);
